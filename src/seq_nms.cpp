954310dcd73dd8bbbedc7b221020c62bf698c809
MrParosk <erik.parlstrand@gmail.com>
2022-04-14 22:18:05 +0200

    Added iou calculation

--- src/seq_nms.cpp
+++ src/seq_nms.cpp
@@ -1,11 +1,40 @@
 #include "seq_nms.h"
 
+
+using namespace torch::indexing;
+
+
 torch::Tensor calculate_area(const torch::Tensor& boxes)
 {
-    auto x1 = boxes.index({torch::indexing::Slice(), torch::indexing::Slice(), 0});
-    auto y1 = boxes.index({torch::indexing::Slice(), torch::indexing::Slice(), 1});
-    auto x2 = boxes.index({torch::indexing::Slice(), torch::indexing::Slice(), 2});
-    auto y2 = boxes.index({torch::indexing::Slice(), torch::indexing::Slice(), 3});
+    auto x1 = boxes.index({Slice(), Slice(), 0});
+    auto y1 = boxes.index({Slice(), Slice(), 1});
+    auto x2 = boxes.index({Slice(), Slice(), 2});
+    auto y2 = boxes.index({Slice(), Slice(), 3});
     auto areas = (x2 - x1) * (y2 - y1);
     return areas;
+}
+
+torch::Tensor calculate_iou_given_area(
+    const torch::Tensor& boxes_a,
+    const torch::Tensor& boxes_b,
+    const torch::Tensor& areas_a,
+    const torch::Tensor& areas_b)
+{
+
+    auto max_xy = torch::min(
+        boxes_a.index({Slice(), None, Slice(2, None)}),
+        boxes_b.index({None, Slice(), Slice(2, None)})
+    );
+
+    auto min_xy = torch::max(
+        boxes_a.index({Slice(), None, Slice(None, 2)}),
+        boxes_b.index({None, Slice(), Slice(None, 2)})
+    );
+
+    auto inter = torch::clamp(max_xy - min_xy, /*min=*/ 0.0);
+    auto intersection = inter.index({Slice(), Slice(), 0}) * inter.index({Slice(), Slice(), 1});
+
+    auto union_ = areas_a.unsqueeze(1) + areas_b.unsqueeze(0) - intersection;    
+    auto iou = torch::div(intersection, union_);
+    return iou;
 }
--- src/seq_nms.h
+++ src/seq_nms.h
@@ -1,4 +1,12 @@
 #pragma once
 #include <torch/torch.h>
 
+
 torch::Tensor calculate_area(const torch::Tensor &boxes);
+
+torch::Tensor calculate_iou_given_area(
+    const torch::Tensor& boxes_a,
+    const torch::Tensor& boxes_b,
+    const torch::Tensor& aread_a,
+    const torch::Tensor& areas_b
+);
