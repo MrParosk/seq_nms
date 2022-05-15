1b3ad10c4e2613c16b5e0fbd77425e700047d896
MrParosk <erik.parlstrand@gmail.com>
2022-05-15 20:18:59 +0200

    changed from vector to tensor for scores

--- src/sequence_utils.cpp
+++ src/sequence_utils.cpp
@@ -12,19 +12,17 @@ std::tuple<int, std::vector<int>, float> find_highest_score_sequence(const std::
     std::vector<int> best_sequence;
     int sequence_frame_index = 0;
 
-    for (int s_idx = 0; s_idx < sequences.size(); s_idx++) {
-        score_indicies_list frame_sequences = sequences[s_idx];
+    for (int f_idx = 0; f_idx < sequences.size(); f_idx++) {
+        score_indicies_list frame_sequences = sequences[f_idx];
 
-        if (frame_sequences.size() == 0) {
-            continue;
+        if (frame_sequences.size() == 0) {continue;}
+
+        auto score_tensor = torch::empty(frame_sequences.size(), {torch::kFloat32});
+        for (int i = 0; i < frame_sequences.size(); i++) {
+            score_tensor.index({i}) = std::get<0>(frame_sequences[i]);
         }
 
-        std::vector<float> score_list;
-        for (score_indicies s : frame_sequences) {
-            score_list.push_back(std::get<0>(s));
-        }
-
-        int max_idx = torch::argmax(torch::tensor(score_list)).item<int>();
+        int max_idx = torch::argmax(score_tensor).item<int>();
 
         if (std::get<0>(frame_sequences[max_idx]) > best_score) {
             best_score = std::get<0>(frame_sequences[max_idx]);
@@ -32,7 +30,7 @@ std::tuple<int, std::vector<int>, float> find_highest_score_sequence(const std::
             best_sequence = std::get<1>(frame_sequences[max_idx]);
             std::reverse(best_sequence.begin(), best_sequence.end());
 
-            sequence_frame_index = s_idx;
+            sequence_frame_index = f_idx;
         }
     }
 
@@ -78,13 +76,15 @@ std::tuple<int, std::vector<int>, float> find_best_sequence(const box_seq_t& box
                 // here we use box_edges to index used_in_sequence and mark boxes in corresponding frame t+1
                 // as part of a sequence since we have links to them and can always make a better max path by making it longer
                 // (score >= 0.0)
-                std::vector<float> score_list;
-                for (int e_idx : box_edges) {
+
+                auto score_tensor = torch::empty(box_edges.size(), {torch::kFloat32});
+                for (int i = 0; i < box_edges.size(); i++) {
+                    int e_idx = box_edges[i];
                     used_in_sequence.index({e_idx}) = true;
-                    score_list.push_back(std::get<0>(max_scores_paths.back()[e_idx]));
+                    score_tensor.index({i}) = std::get<0>(max_scores_paths.back()[e_idx]);
                 }
 
-                int prev_idx = torch::argmax(torch::tensor(score_list)).item<int>();
+                int prev_idx = torch::argmax(score_tensor).item<int>();
                 float score_so_far = std::get<0>(max_scores_paths.back()[box_edges[prev_idx]]);
                 std::vector<int> path_so_far = std::get<1>(max_scores_paths.back()[box_edges[prev_idx]]);
                 path_so_far.push_back(box_idx);
