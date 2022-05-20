94bd27a516b63e4f0f7260920d06cb5747c0e098
MrParosk <erik.parlstrand@gmail.com>
2022-05-20 11:13:07 +0200

    Created folders for cpp files

--- scripts/run_cpp_tests.sh
+++ scripts/run_cpp_tests.sh
@@ -6,4 +6,4 @@ current_dir=$(pwd)
 mkdir -p build
 (cd build && cmake -DCMAKE_PREFIX_PATH="$current_dir/libtorch" -DCMAKE_BUILD_TYPE=Debug .. && make -j)
 
-./build/tests/run_tests
+./build/tests/cpp/run_tests
